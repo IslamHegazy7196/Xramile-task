10352859e4a8aba59de8996c46fcca480a5893d8
IslamHegazy7196 <islamfathihegazy04@gmai.com>
2022-03-21 04:47:02 +0200

    initial structure

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+
+export { Home, Products, SingleProduct, Cart, Error };
