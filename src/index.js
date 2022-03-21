f606f817b99a208c8165e472534a90f255dde37c
IslamHegazy7196 <islamfathihegazy04@gmai.com>
2022-03-21 21:53:23 +0200

    add basic navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <ProductsProvider>
+    <App />
+  </ProductsProvider>,
+  document.getElementById("root")
+);
